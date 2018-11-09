13e8a35fb13d61ff3c7c7f687ab534cf19cd116c
Michael Wender <michael@michaelwender.com>
2018-11-09 08:40:08 -0500

    Initial Spacial theme setup

--- lib/fns/enqueues.php
+++ lib/fns/enqueues.php
@@ -3,7 +3,7 @@
 namespace TriadSemiCart\enqueues;
 
 function enqueue_scripts(){
-  wp_enqueue_style( 'spacial', get_stylesheet_directory_uri() . '/static/spacial/theme.min.css', null, filemtime( get_stylesheet_directory() . '/static/spacial/theme.min.css' ) );
-  wp_enqueue_script('spacial', get_stylesheet_directory_uri() . '/static/spacial/theme.min.js', ['jquery'], filemtime( get_stylesheet_directory() . '/static/spacial/theme.min.js' ), true );
+  wp_enqueue_style( 'spacial', get_stylesheet_directory_uri() . '/dist/theme.min.css', null, filemtime( get_stylesheet_directory() . '/dist/theme.min.css' ) );
+  wp_enqueue_script('spacial', get_stylesheet_directory_uri() . '/dist/theme.min.js', ['jquery'], filemtime( get_stylesheet_directory() . '/dist/theme.min.js' ), true );
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts' );
