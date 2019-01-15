f681e09a7f68b95949050cccbc954741c5d823bf
Michael Wender <michael@michaelwender.com>
2019-01-15 09:34:18 -0500

    Misc content edits

--- static/site.js
+++ static/site.js
@@ -2,6 +2,9 @@ jQuery( document ).ready( function( $ ) {
 
   // Your JavaScript goes here
   $('#engineering-expertise').DataTable({
-    "lengthMenu": [40,80,100,150,200]
+    "lengthMenu": [20,40,80,100]
+  });
+  $('#reps-and-distributors').DataTable({
+    "lengthMenu": [20,40,80,100]
   });
 });
